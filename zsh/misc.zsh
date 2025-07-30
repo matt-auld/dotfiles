644b0e1b36ae42e25aeaafa6eb4b3f88b9e1c98a
Matthew Auld <matthew.auld@intel.com>
2025-07-30 10:01:51 +0100

    gl/gb
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -35,3 +35,60 @@ install-kernel() {
 
         sudo grub2-mkconfig --output=/boot/grub2/grub.cfg
 }
+
+function gl() {
+        git log --max-count=10000 --graph --color --format='%C(red)%h %Creset%s %C(green)(%cr)%C(red)%d%Creset %C(bold)%an%Creset' $1 | fzf \
+          -e \
+          --ansi \
+          --color=light \
+          --reverse \
+          --no-sort \
+          --preview-window=hidden \
+          --preview='
+            hash=$(echo {} | grep -o "[a-f0-9]*" | sed -n "1p") \
+            && [[ $hash != "" ]] \
+            && git show --color $hash | diff-so-fancy
+            ' \
+          --bind='enter:execute(
+            hash=$(echo {} | grep -o "[a-f0-9]*" | sed -n "1p") \
+            && [[ $hash != "" ]] \
+            && sh -c "git show $hash | diff-so-fancy | less -R"
+            )' \
+          --bind='alt-c:execute(
+            hash=$(echo {} | grep -o "[a-f0-9]*" | sed -n "1p") \
+            && [[ $hash != "" ]] \
+            && git checkout $hash
+            )+abort' \
+          --bind='alt-r:execute(
+            hash=$(echo {} | grep -o "[a-f0-9]*" | sed -n "1p") \
+            && [[ $hash != "" ]] \
+            && git reset $hash
+            )+abort' \
+          --bind='alt-i:execute(
+            hash=$(echo {} | grep -o "[a-f0-9]*" | sed -n "1p") \
+            && [[ $hash != "" ]] \
+            && git rebase -i $hash
+            )+abort' \
+          --bind='alt-p:toggle-preview' \
+          --bind='alt-h:toggle-header' \
+          --header-first \
+          --header '
+          > ENTER | <show>
+          > ALT-C | <checkout> the commit
+          > ALT-R | <reset> to the commit
+          > ALT-I | <rebase -i> from the commit'
+}
+
+function gb() {
+        git branch | fzf \
+          --ansi \
+          --color=light \
+          --reverse \
+          --no-sort \
+          --preview='git log {-1} --max-count=100 --graph --color --format="%C(red)%h %Creset%s %C(green)(%cr)%C(red)%d%Creset %C(bold)%an%Creset"' \
+          --bind='enter:execute(
+            git switch {-1}
+            )+abort' \
+          --header '
+          > ENTER | <switch> to the branch'
+}
